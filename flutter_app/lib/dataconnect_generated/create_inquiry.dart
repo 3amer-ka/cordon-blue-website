3b036562ab81a89cde188d249574429a388cbf41
3amer-ka <211031140+3amer-ka@users.noreply.github.com>
2026-02-11 23:39:07 +0100

    Refactor DataConnect generated Dart files to fix late final errors and adhere to lowerCamelCase conventions

--- flutter_app/lib/dataconnect_generated/create_inquiry.dart
+++ flutter_app/lib/dataconnect_generated/create_inquiry.dart
@@ -4,53 +4,75 @@ class CreateInquiryVariablesBuilder {
   String name;
   String email;
   String message;
-  Optional<String> _phoneNumber = Optional.optional(nativeFromJson, nativeToJson);
+  Optional<String> _phoneNumber = Optional.optional(
+    nativeFromJson,
+    nativeToJson,
+  );
   Optional<String> _subject = Optional.optional(nativeFromJson, nativeToJson);
 
-  final FirebaseDataConnect _dataConnect;  CreateInquiryVariablesBuilder phoneNumber(String? t) {
-   _phoneNumber.value = t;
-   return this;
-  }
-  CreateInquiryVariablesBuilder subject(String? t) {
-   _subject.value = t;
-   return this;
+  final FirebaseDataConnect _dataConnect;
+  CreateInquiryVariablesBuilder phoneNumber(String? t) {
+    _phoneNumber.value = t;
+    return this;
   }
 
-  CreateInquiryVariablesBuilder(this._dataConnect, {required  this.name,required  this.email,required  this.message,});
-  Deserializer<CreateInquiryData> dataDeserializer = (dynamic json)  => CreateInquiryData.fromJson(jsonDecode(json));
-  Serializer<CreateInquiryVariables> varsSerializer = (CreateInquiryVariables vars) => jsonEncode(vars.toJson());
+  CreateInquiryVariablesBuilder subject(String? t) {
+    _subject.value = t;
+    return this;
+  }
+
+  CreateInquiryVariablesBuilder(
+    this._dataConnect, {
+    required this.name,
+    required this.email,
+    required this.message,
+  });
+  Deserializer<CreateInquiryData> dataDeserializer = (dynamic json) =>
+      CreateInquiryData.fromJson(jsonDecode(json));
+  Serializer<CreateInquiryVariables> varsSerializer =
+      (CreateInquiryVariables vars) => jsonEncode(vars.toJson());
   Future<OperationResult<CreateInquiryData, CreateInquiryVariables>> execute() {
     return ref().execute();
   }
 
   MutationRef<CreateInquiryData, CreateInquiryVariables> ref() {
-    CreateInquiryVariables vars= CreateInquiryVariables(name: name,email: email,message: message,phoneNumber: _phoneNumber,subject: _subject,);
-    return _dataConnect.mutation("CreateInquiry", dataDeserializer, varsSerializer, vars);
+    CreateInquiryVariables vars = CreateInquiryVariables(
+      name: name,
+      email: email,
+      message: message,
+      phoneNumber: _phoneNumber,
+      subject: _subject,
+    );
+    return _dataConnect.mutation(
+      "CreateInquiry",
+      dataDeserializer,
+      varsSerializer,
+      vars,
+    );
   }
 }
 
 @immutable
 class CreateInquiryInquiryInsert {
   final String id;
-  CreateInquiryInquiryInsert.fromJson(dynamic json):
-  
-  id = nativeFromJson<String>(json['id']);
+  CreateInquiryInquiryInsert.fromJson(dynamic json)
+    : id = nativeFromJson<String>(json['id']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
-    final CreateInquiryInquiryInsert otherTyped = other as CreateInquiryInquiryInsert;
+    final CreateInquiryInquiryInsert otherTyped =
+        other as CreateInquiryInquiryInsert;
     return id == otherTyped.id;
-    
   }
+
   @override
   int get hashCode => id.hashCode;
-  
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
@@ -58,43 +80,39 @@ class CreateInquiryInquiryInsert {
     return json;
   }
 
-  CreateInquiryInquiryInsert({
-    required this.id,
-  });
+  CreateInquiryInquiryInsert({required this.id});
 }
 
 @immutable
 class CreateInquiryData {
-  final CreateInquiryInquiryInsert inquiry_insert;
-  CreateInquiryData.fromJson(dynamic json):
-  
-  inquiry_insert = CreateInquiryInquiryInsert.fromJson(json['inquiry_insert']);
+  final CreateInquiryInquiryInsert inquiryInsert;
+  CreateInquiryData.fromJson(dynamic json)
+    : inquiryInsert = CreateInquiryInquiryInsert.fromJson(
+        json['inquiry_insert'],
+      );
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
     final CreateInquiryData otherTyped = other as CreateInquiryData;
-    return inquiry_insert == otherTyped.inquiry_insert;
-    
+    return inquiryInsert == otherTyped.inquiryInsert;
   }
+
   @override
-  int get hashCode => inquiry_insert.hashCode;
-  
+  int get hashCode => inquiryInsert.hashCode;
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
-    json['inquiry_insert'] = inquiry_insert.toJson();
+    json['inquiry_insert'] = inquiryInsert.toJson();
     return json;
   }
 
-  CreateInquiryData({
-    required this.inquiry_insert,
-  });
+  CreateInquiryData({required this.inquiryInsert});
 }
 
 @immutable
@@ -102,57 +120,58 @@ class CreateInquiryVariables {
   final String name;
   final String email;
   final String message;
-  late final Optional<String>phoneNumber;
-  late final Optional<String>subject;
-  @Deprecated('fromJson is deprecated for Variable classes as they are no longer required for deserialization.')
-  CreateInquiryVariables.fromJson(Map<String, dynamic> json):
-  
-  name = nativeFromJson<String>(json['name']),
-  email = nativeFromJson<String>(json['email']),
-  message = nativeFromJson<String>(json['message']) {
-  
-  
-  
-  
-  
-    phoneNumber = Optional.optional(nativeFromJson, nativeToJson);
-    phoneNumber.value = json['phoneNumber'] == null ? null : nativeFromJson<String>(json['phoneNumber']);
-  
-  
-    subject = Optional.optional(nativeFromJson, nativeToJson);
-    subject.value = json['subject'] == null ? null : nativeFromJson<String>(json['subject']);
-  
-  }
+  final Optional<String> phoneNumber;
+  final Optional<String> subject;
+  @Deprecated(
+    'fromJson is deprecated for Variable classes as they are no longer required for deserialization.',
+  )
+  CreateInquiryVariables.fromJson(Map<String, dynamic> json)
+    : name = nativeFromJson<String>(json['name']),
+      email = nativeFromJson<String>(json['email']),
+      message = nativeFromJson<String>(json['message']),
+      phoneNumber = Optional.optional(nativeFromJson, nativeToJson)
+        ..value = json['phoneNumber'] == null
+            ? null
+            : nativeFromJson<String>(json['phoneNumber']),
+      subject = Optional.optional(nativeFromJson, nativeToJson)
+        ..value = json['subject'] == null
+            ? null
+            : nativeFromJson<String>(json['subject']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
     final CreateInquiryVariables otherTyped = other as CreateInquiryVariables;
-    return name == otherTyped.name && 
-    email == otherTyped.email && 
-    message == otherTyped.message && 
-    phoneNumber == otherTyped.phoneNumber && 
-    subject == otherTyped.subject;
-    
+    return name == otherTyped.name &&
+        email == otherTyped.email &&
+        message == otherTyped.message &&
+        phoneNumber == otherTyped.phoneNumber &&
+        subject == otherTyped.subject;
   }
+
   @override
-  int get hashCode => Object.hashAll([name.hashCode, email.hashCode, message.hashCode, phoneNumber.hashCode, subject.hashCode]);
-  
+  int get hashCode => Object.hashAll([
+    name.hashCode,
+    email.hashCode,
+    message.hashCode,
+    phoneNumber.hashCode,
+    subject.hashCode,
+  ]);
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
     json['name'] = nativeToJson<String>(name);
     json['email'] = nativeToJson<String>(email);
     json['message'] = nativeToJson<String>(message);
-    if(phoneNumber.state == OptionalState.set) {
+    if (phoneNumber.state == OptionalState.set) {
       json['phoneNumber'] = phoneNumber.toJson();
     }
-    if(subject.state == OptionalState.set) {
+    if (subject.state == OptionalState.set) {
       json['subject'] = subject.toJson();
     }
     return json;
@@ -166,4 +185,3 @@ class CreateInquiryVariables {
     required this.subject,
   });
 }
-
--- flutter_app/lib/dataconnect_generated/update_project_status.dart
+++ flutter_app/lib/dataconnect_generated/update_project_status.dart
@@ -5,41 +5,55 @@ class UpdateProjectStatusVariablesBuilder {
   String status;
 
   final FirebaseDataConnect _dataConnect;
-  UpdateProjectStatusVariablesBuilder(this._dataConnect, {required  this.id,required  this.status,});
-  Deserializer<UpdateProjectStatusData> dataDeserializer = (dynamic json)  => UpdateProjectStatusData.fromJson(jsonDecode(json));
-  Serializer<UpdateProjectStatusVariables> varsSerializer = (UpdateProjectStatusVariables vars) => jsonEncode(vars.toJson());
-  Future<OperationResult<UpdateProjectStatusData, UpdateProjectStatusVariables>> execute() {
+  UpdateProjectStatusVariablesBuilder(
+    this._dataConnect, {
+    required this.id,
+    required this.status,
+  });
+  Deserializer<UpdateProjectStatusData> dataDeserializer = (dynamic json) =>
+      UpdateProjectStatusData.fromJson(jsonDecode(json));
+  Serializer<UpdateProjectStatusVariables> varsSerializer =
+      (UpdateProjectStatusVariables vars) => jsonEncode(vars.toJson());
+  Future<OperationResult<UpdateProjectStatusData, UpdateProjectStatusVariables>>
+  execute() {
     return ref().execute();
   }
 
   MutationRef<UpdateProjectStatusData, UpdateProjectStatusVariables> ref() {
-    UpdateProjectStatusVariables vars= UpdateProjectStatusVariables(id: id,status: status,);
-    return _dataConnect.mutation("UpdateProjectStatus", dataDeserializer, varsSerializer, vars);
+    UpdateProjectStatusVariables vars = UpdateProjectStatusVariables(
+      id: id,
+      status: status,
+    );
+    return _dataConnect.mutation(
+      "UpdateProjectStatus",
+      dataDeserializer,
+      varsSerializer,
+      vars,
+    );
   }
 }
 
 @immutable
 class UpdateProjectStatusProjectUpdate {
   final String id;
-  UpdateProjectStatusProjectUpdate.fromJson(dynamic json):
-  
-  id = nativeFromJson<String>(json['id']);
+  UpdateProjectStatusProjectUpdate.fromJson(dynamic json)
+    : id = nativeFromJson<String>(json['id']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
-    final UpdateProjectStatusProjectUpdate otherTyped = other as UpdateProjectStatusProjectUpdate;
+    final UpdateProjectStatusProjectUpdate otherTyped =
+        other as UpdateProjectStatusProjectUpdate;
     return id == otherTyped.id;
-    
   }
+
   @override
   int get hashCode => id.hashCode;
-  
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
@@ -47,73 +61,69 @@ class UpdateProjectStatusProjectUpdate {
     return json;
   }
 
-  UpdateProjectStatusProjectUpdate({
-    required this.id,
-  });
+  UpdateProjectStatusProjectUpdate({required this.id});
 }
 
 @immutable
 class UpdateProjectStatusData {
-  final UpdateProjectStatusProjectUpdate? project_update;
-  UpdateProjectStatusData.fromJson(dynamic json):
-  
-  project_update = json['project_update'] == null ? null : UpdateProjectStatusProjectUpdate.fromJson(json['project_update']);
+  final UpdateProjectStatusProjectUpdate? projectUpdate;
+  UpdateProjectStatusData.fromJson(dynamic json)
+    : projectUpdate = json['project_update'] == null
+          ? null
+          : UpdateProjectStatusProjectUpdate.fromJson(json['project_update']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
     final UpdateProjectStatusData otherTyped = other as UpdateProjectStatusData;
-    return project_update == otherTyped.project_update;
-    
+    return projectUpdate == otherTyped.projectUpdate;
   }
+
   @override
-  int get hashCode => project_update.hashCode;
-  
+  int get hashCode => projectUpdate.hashCode;
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
-    if (project_update != null) {
-      json['project_update'] = project_update!.toJson();
+    if (projectUpdate != null) {
+      json['project_update'] = projectUpdate!.toJson();
     }
     return json;
   }
 
-  UpdateProjectStatusData({
-    this.project_update,
-  });
+  UpdateProjectStatusData({this.projectUpdate});
 }
 
 @immutable
 class UpdateProjectStatusVariables {
   final String id;
   final String status;
-  @Deprecated('fromJson is deprecated for Variable classes as they are no longer required for deserialization.')
-  UpdateProjectStatusVariables.fromJson(Map<String, dynamic> json):
-  
-  id = nativeFromJson<String>(json['id']),
-  status = nativeFromJson<String>(json['status']);
+  @Deprecated(
+    'fromJson is deprecated for Variable classes as they are no longer required for deserialization.',
+  )
+  UpdateProjectStatusVariables.fromJson(Map<String, dynamic> json)
+    : id = nativeFromJson<String>(json['id']),
+      status = nativeFromJson<String>(json['status']);
   @override
   bool operator ==(Object other) {
-    if(identical(this, other)) {
+    if (identical(this, other)) {
       return true;
     }
-    if(other.runtimeType != runtimeType) {
+    if (other.runtimeType != runtimeType) {
       return false;
     }
 
-    final UpdateProjectStatusVariables otherTyped = other as UpdateProjectStatusVariables;
-    return id == otherTyped.id && 
-    status == otherTyped.status;
-    
+    final UpdateProjectStatusVariables otherTyped =
+        other as UpdateProjectStatusVariables;
+    return id == otherTyped.id && status == otherTyped.status;
   }
+
   @override
   int get hashCode => Object.hashAll([id.hashCode, status.hashCode]);
-  
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> json = {};
@@ -122,9 +132,5 @@ class UpdateProjectStatusVariables {
     return json;
   }
 
-  UpdateProjectStatusVariables({
-    required this.id,
-    required this.status,
-  });
+  UpdateProjectStatusVariables({required this.id, required this.status});
 }
-
